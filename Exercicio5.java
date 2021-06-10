0d65a6cf1f9bb75032301c32fa395181012f0ced
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-06-10 13:31:37 -0300

    Create Exercicio5.java

---
+++ Exercicio5.java
@@ -0,0 +1,19 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+
+	public static void main(String[]args) {
+		int numero, res;
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		numero=sc.nextInt();
+		res = numero - 1;
+		
+		System.out.println("O número anetecessor ao digitado é:"+res);
+	
+	}
+	
+}
