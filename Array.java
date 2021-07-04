32710d5bb764b1da49e3e95ab4b1d8d169e964bd
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:35:40 -0300

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,17 @@
+package exercicio;
+
+public class ArrayMatriz {
+
+public static void main(String[] args) {
+		
+		int [][] numero= {{10, 12, 14},{4, 5, 6},{36, 23, 55,}};
+		
+		int x = numero[1][0];
+		int y = numero[1][1];
+		int z = numero[2][0];
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+
+	}
+}
